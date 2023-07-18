eec21cc8360b934b0f4ba4620c864e3b64c61752
Joan Marti <joanmartigamez@gmail.com [core]>
2023-07-18 20:43:16 +0200

    Characters Queries Done!

--- routes/api/characters.js
+++ routes/api/characters.js
@@ -1,10 +1,32 @@
 const router = require('express').Router();
+const { getAll, getByName, getByShowId } = require('../../models/characters.model')
 
-// GET Queries 
+// GET Queries:
 
+// Get all characters
 router.get('/', async (req, res) => {
     try {
-        const [result] = await db.query(`SELECT * FROM audiovisual_production.characters`);
+        const [result] = await getAll();
+        res.json(result)
+    } catch (error) {
+        res.json({ 'fatal': error.message })
+    }
+});
+
+// Get characters by name
+router.get('/:characterName', async (req, res) => {
+    try {
+        const [result] = await getByName(req.params.characterName);
+        res.json(result)
+    } catch (error) {
+        res.json({ 'fatal': error.message })
+    }
+});
+
+// Get characters by showId
+router.get('/shows/:showId', async (req, res) => {
+    try {
+        const [result] = await getByShowId(req.params.showId);
         console.log(result);
         res.json(result)
     } catch (error) {
