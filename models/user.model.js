5ffec4aa65dd2b29c87a588dacb83c0860107dc4
Sofia <paezsofiam@gmail.com>
2023-07-19 18:06:55 +0200

    Queries de User

--- models/user.model.js
+++ models/user.model.js
@@ -1,10 +1,26 @@
-// **********Esto es un ejemplo de guia************
 
-// const tableDb = 'dbName.tableName';
+const usersDb = 'audiovisual_production.users';
 
-// const getAll = () => {
-//     return db.query(`SELECT * FROM ${tableDb}`);
-// };
+const getAll = () => {
+    return db.query(`SELECT * FROM ${usersDb}`);
+ };
+const getById = (userID) => {
+    return db.query(`SELECT * FROM ${usersDb} WHERE id=?`, [userID])
+}; 
 
+/*const getRole = (userRole) => {
+    return db.query(`SELECT * FROM ${usersDb} WHERE role=?`, [userRole])
+}; */ 
 
-// module.exports = { getAll, getById, create, deleteUserById, update }
+const create = ({name, surname, age, dni, email, phone, address, password, role}) => { return db.query(`INSERT INTO ${usersDb}(name,surname,age,dni,email,phone, address,password,role)VALUES(?,?,?,?,?,?,?,?,?)`, [name, surname, age, dni, email, phone, address, password, role])
+};
+
+const update = (userID, { name, surname, age, dni, email, phone, address, password, role }) => {
+    return db.query(`UPDATE ${usersDb} SET users.name= ?, users.surname= ?, users.age= ?, users.dni= ?, users.email= ?,users.phone= ?, users.address= ?, users.password= ?, users.role= ? WHERE users.id= ?;`, [name, surname, age, dni, email, phone, address, password, role, userID]);
+}; 
+
+const deleteUserById = (userID) => {
+    return db.query(`DELETE FROM ${usersDb} WHERE id=?`, [userID]); 
+}
+
+module.exports = { getAll, getById, create, update, deleteUserById}
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,20 +1,70 @@
+const { getById, create, update, deleteUserById, } = require('../../models/user.model');
+
 const router = require('express').Router();
-
-// GET Queries 
-
-// router.get('/', (req, res) => {
-//     res.end('Esto es users.js');
-// });
 
 
 router.get('/', async (req, res) => {
     try {
         const [result] = await db.query(`SELECT * FROM audiovisual_production.users`);
-        console.log(result);
+        console.log(result)
         res.json(result)
     } catch (error) {
         res.json({ 'fatal': error.message })
     }
 })
+
+router.get ('/:userID', async (req, res) => {
+    try {
+        let id = Number(req.params.id); 
+
+        const [result] = await getById(req.params.userID)
+        res.json(result)
+    } catch (error) {
+        res.json({ 'fatal': error.message })
+    }
+})
+
+router.post ('/', async (req, res) => {
+    try {
+        const [result] = await create(req.body)
+        res.json (result)
+    }
+    catch (error) {
+        res.json({ 'fatal': error.message })
+    }
+})
+
+router.put ('/update/:id', async (req, res) => {
+    try {
+        const [result] = await update(Number(req.params.id), req.body)
+        const [result_final] = await getById(Number(req.params.id)); 
+        res.json(result_final);
+    
+    } catch (error) {
+        res.json({'fatal': error.message})
+        
+    }
+})
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const [result] = await deleteUserById(Number(req.params.id));
+        res.json(result)
+    }
+    catch (error) {
+        res.json({ 'fatal': error.message });
+    }
+})
+
+/*router.get('/users/:role', (req, res) => { 
+    cont role = req.params.role; 
+    try {
+        const [result] = await getByRole(req.params.userID)
+        res.json(result)
+    } catch (error) {
+        res.json({ 'fatal': error.message })
+    }
+})*/
+
 
 module.exports = router;
